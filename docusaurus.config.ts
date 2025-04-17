a8cb870f4ebe32b3d61ac0d70eb9f8ff724ddacb
brendanwong-web <69905640+brendanwong-web@users.noreply.github.com>
2025-04-17 14:46:13 +0800

    changed dir

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -68,17 +68,17 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: '',
       logo: {
         alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        src: 'img/logo.png',
       },
       items: [
         {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Documentation',
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
